6964a83f127468cb2b6729ff508c59c7c15ec5a2
Cami Hayes <camihayes72@gmail.com>
2017-02-22 16:27:09 +0000

    added exercise model and form partial

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -6,15 +6,15 @@ RSpec.feature "Creating Exercise" do
         login_as(@john)
     end 
     
-    scenario "with valid inouts" do
+    scenario "with valid inputs" do
         visit "/"
         
         click_link "My Lounge"
         click_link "New Workout"
         expect(page).to have_link("Back")
         
-        fill_in "Duration", with: 70 
-        fill_in "Workout Details", with: "Trail Running"
+        fill_in "Duration", with: 70
+        fill_in "Workout details", with: "Trail Running"
         fill_in "Activity date", with: "2017-02-21"
         click_button "Create Exercise" 
         expect(page).to have_content("Exercise has been created")
@@ -23,4 +23,5 @@ RSpec.feature "Creating Exercise" do
         expect(current_path).to eq(user_exercise_path(@john, exercise))
         expect(exercise.user_id).to eq(@john.id)
     end
+    
 end
