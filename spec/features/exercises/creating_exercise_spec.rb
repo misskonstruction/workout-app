8c60f3809805db41176b71e2d3ac24e698ce3ebc
Cami Hayes <camihayes72@gmail.com>
2017-02-27 10:49:30 +0000

    added validations to exercises

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -1,27 +1,38 @@
 require "rails_helper"
 
 RSpec.feature "Creating Exercise" do
-    before do
-        @john = User.create!(email: "john@example.com", password: "password")
-        login_as(@john)
-    end 
+  before do
+  @john = User.create!(email: "john@example.com", password: "password")
+  login_as(@john)
+  
+  visit "/"
+  
+  click_link "My Lounge"
+  click_link "New Workout"
+  expect(page).to have_link("Back")
+end 
     
-    scenario "with valid inputs" do
-        visit "/"
+  scenario "with valid inputs" do
+  fill_in "Duration", with: 70
+  fill_in "Workout details", with: "Trail Running"
+  fill_in "Activity date", with: "2017-02-21"
+  click_button "Create Exercise" 
+  expect(page).to have_content("Exercise has been created")
         
-        click_link "My Lounge"
-        click_link "New Workout"
-        expect(page).to have_link("Back")
-        
-        fill_in "Duration", with: 70
-        fill_in "Workout details", with: "Trail Running"
-        fill_in "Activity date", with: "2017-02-21"
-        click_button "Create Exercise" 
-        expect(page).to have_content("Exercise has been created")
-        
-        exercise = Exercise.last
-        expect(current_path).to eq(user_exercise_path(@john, exercise))
-        expect(exercise.user_id).to eq(@john.id)
-    end
+  exercise = Exercise.last
+  expect(current_path).to eq(user_exercise_path(@john, exercise))
+  expect(exercise.user_id).to eq(@john.id)
+end
     
+  scenario "with invalid inputs" do
+  fill_in "Duration", with: ""
+  fill_in "Workout details", with: ""
+  fill_in "Activity date", with: ""
+  click_button "Create Exercise"
+        
+  expect(page).to have_content("Exercise has not been created")
+  expect(page).to have_content("Duration in min is not a number")
+  expect(page).to have_content("Workout details cant be blank")
+  expect(page).to have_content("Activity date can't be blank")
+  end
 end
