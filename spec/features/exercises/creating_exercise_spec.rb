66850176f95697d1c1625699d3b5b444b6944017
Cami Hayes <camihayes72@gmail.com>
2017-02-27 12:01:56 +0000

    display user's workout on index page

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -32,7 +32,7 @@ end
         
   expect(page).to have_content("Exercise has not been created")
   expect(page).to have_content("Duration in min is not a number")
-  expect(page).to have_content("Workout details cant be blank")
+  expect(page).to have_content("Workout details can't be blank")
   expect(page).to have_content("Activity date can't be blank")
   end
 end
---
+++ spec/features/exercises/listing_exercises_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+RSpec.feature "Listing Exercise" do
+  before do
+  @john = User.create!(email: "john@example.com", password: "password")
+  login_as(@john)
+  
+  @e1 = @john.exercises.create(duration_in_min: 45,
+                               workout: "Trail Run",
+                               workout_date: Date.today)
+                               
+  @e2 = @john.exercises.create(duration_in_min: 55,
+                               workout: "Run with Dogs",
+                               workout_date: 2.days.ago)
+  
+  end
+  
+  scenario "shows user's workout for last 7 days" do
+    visit '/'
+    
+    click_link "My Lounge"
+  
+    expect(page).to have_content("@e1.duration_in_min")
+    expect(page).to have_content("@e1.workout")
+    expect(page).to have_content("@e1.workout_date")
+    
+    expect(page).to have_content("@e2.duration_in_min")
+    expect(page).to have_content("@e2.workout")
+    expect(page).to have_content("@e2.workout_date")
+    
+    #expect(page).to have_content("@e3.duration_in_min")
+    #expect(page).to have_content("@e3.workout")
+    #expect(page).to have_content("@e3.workout_date")
+  end
+end
