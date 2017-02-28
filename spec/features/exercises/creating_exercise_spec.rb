39f9d99bba4e7f3cfc17026ab90160929ed48102
Cami Hayes <camihayes72@gmail.com>
2017-02-28 11:06:20 +0000

    removed hardcoded date from creating exercise feature spec

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -15,7 +15,7 @@ end
   scenario "with valid inputs" do
   fill_in "Duration", with: 70
   fill_in "Workout details", with: "Trail Running"
-  fill_in "Activity date", with: "2017-02-21"
+  fill_in "Activity date", with: 3.days.ago
   click_button "Create Exercise" 
   expect(page).to have_content("Exercise has been created")
         
