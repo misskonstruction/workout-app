21e769ca659a38218653bc7d829cf45b873251ba
Cami Hayes <camihayes72@gmail.com>
2017-02-28 10:34:16 +0000

    display msg if no exercise created

--- spec/features/exercises/listing_exercises_spec.rb
+++ spec/features/exercises/listing_exercises_spec.rb
@@ -20,16 +20,26 @@ RSpec.feature "Listing Exercise" do
     
     click_link "My Lounge"
   
-    expect(page).to have_content("@e1.duration_in_min")
-    expect(page).to have_content("@e1.workout")
-    expect(page).to have_content("@e1.workout_date")
+    expect(page).to have_content(@e1.duration_in_min)
+    expect(page).to have_content(@e1.workout)
+    expect(page).to have_content(@e1.workout_date)
     
-    expect(page).to have_content("@e2.duration_in_min")
-    expect(page).to have_content("@e2.workout")
-    expect(page).to have_content("@e2.workout_date")
+    expect(page).to have_content(@e2.duration_in_min)
+    expect(page).to have_content(@e2.workout)
+    expect(page).to have_content(@e2.workout_date)
     
-    #expect(page).to have_content("@e3.duration_in_min")
-    #expect(page).to have_content("@e3.workout")
-    #expect(page).to have_content("@e3.workout_date")
+    #expect(page).to have_content(@e3.duration_in_min)
+    #expect(page).to have_content(@e3.workout)
+    #expect(page).to have_content(@e3.workout_date)
+  end
+  
+  scenario "shows no exercise if none created" do
+    @john.exercises.delete_all
+    
+    visit '/'
+    
+    click_link 'My Lounge'
+    
+    expect(page).to have_content('No Workouts Yet')
   end
 end
