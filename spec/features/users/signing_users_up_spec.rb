584a409ddf8e43fa76ea448525fa8490e5587ddd
Cami Hayes <camihayes72@gmail.com>
2017-03-14 10:43:58 +0000

    create chatroom when user signs up

--- spec/features/users/signing_users_up_spec.rb
+++ spec/features/users/signing_users_up_spec.rb
@@ -14,6 +14,11 @@ RSpec.feature "Users signup" do
         
         expect(page).to have_content("You have signed up successfully.")
         
+        user = User.last
+        room = user.room
+        room_name = user.full_name.split.join('-')
+        expect(room.name).to eq(room_name)
+        
         visit "/"
         expect(page).to have_content("John Doe") 
     end
@@ -22,13 +27,13 @@ RSpec.feature "Users signup" do
         visit "/"
         click_link "Sign up"
         fill_in "First name", with: ""
-        fill_in "Last name", with: ""
+        fill_in "Last name", with: "" 
         fill_in "Email", with: "john@example.com"
         fill_in "Password", with: "password"
         fill_in "Password confirmation", with: "password"
         click_button "Sign up"
     
-        expect(page).to have_content("First name can't be blank")
+        expect(page).to have_content("First name can't be blank") 
         expect(page).to have_content("Last name can't be blank")
     end
 end
